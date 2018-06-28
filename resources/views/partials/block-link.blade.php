71c17eabe368cd8114904ed074e3dabf1d9b2b04
Jaroslaw Goszowski <goszowski@gmail.com>
2018-06-28 14:20:36 +0300

    Transy w przycisku wypelniania briefu

--- resources/views/partials/block-link.blade.php
+++ resources/views/partials/block-link.blade.php
@@ -1,10 +1,10 @@
 
 <a href="/brief" data-aos="fade" class="aos inner-block_wrapp clearfix pl-172 pr-172 hover-green">
 	<div class="inner-block">
-		<p class="title-block_small-title">Co możemy dla ciebie zrobić?</p>
-		<h2 class="title-block_big-title fz48">Wypełnij Brief</h2>
+		<p class="title-block_small-title">{{ t('Co możemy dla ciebie zrobić?') }}</p>
+		<h2 class="title-block_big-title fz48">{{ t('Wypełnij Brief') }}</h2>
 	</div>
 	<div class="inner-block text-right">
-		<span class="has-icon-btn text-uppercase">WYPEŁNIJ</span>
+		<span class="has-icon-btn text-uppercase">{{ t('WYPEŁNIJ') }}</span>
 	</div>
 </a>
